e7c5eedbca24bb9495f8f4f84c1c6c323f56f352
RichieG78 <richard.glynn@Richards-MacBook-Air.local>
2026-06-28 19:53:29 +0200

    new migrations

--- exhibit_guide_pwa/exhibits/admin.py
+++ exhibit_guide_pwa/exhibits/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
+
 from .models import Exhibit, Prospect
 
-# Register your models here.
 
-admin.site.register(Exhibit)
-admin.site.register(Prospect)
+class ProspectInline(admin.TabularInline):
+	model = Prospect
+	extra = 0
+	fields = ('firstname', 'lastname', 'email', 'phone', 'dwell_time')
+	show_change_link = True
+
+
+@admin.register(Exhibit)
+class ExhibitAdmin(admin.ModelAdmin):
+	list_display = ('artwork', 'artist', 'show_name', 'currency', 'price', 'qr_identifier', 'publish_date')
+	list_filter = ('gallery_name', 'currency', 'publish_date')
+	search_fields = ('artwork', 'artist', 'show_name', 'gallery_name')
+	ordering = ('-publish_date',)
+	inlines = (ProspectInline,)
+	readonly_fields = ('id',)
+	fieldsets = (
+		('Core Details', {
+			'fields': ('id', 'gallery_name', 'show_name', 'artwork', 'artist', 'medium')
+		}),
+		('Artwork Specifications', {
+			'fields': ('dimensions_height', 'dimensions_width', 'provenance')
+		}),
+		('Commerce and Delivery', {
+			'fields': ('price', 'currency', 'audio_url', 'video_url', 'image', 'image_url')
+		}),
+		('Publishing', {
+			'fields': ('qr_identifier', 'publish_date', 'user', 'tldr', 'full_text')
+		}),
+	)
+
+
+@admin.register(Prospect)
+class ProspectAdmin(admin.ModelAdmin):
+	list_display = ('firstname', 'lastname', 'email', 'exhibit', 'saved_at', 'dwell_time')
+	list_filter = ('saved_at', 'exhibit')
+	search_fields = ('firstname', 'lastname', 'email', 'phone', 'exhibit__artwork')
+	ordering = ('-saved_at',)
--- exhibit_guide_pwa/exhibits/models.py
+++ exhibit_guide_pwa/exhibits/models.py
@@ -6,23 +6,29 @@ from django.utils import timezone
 class Exhibit(models.Model):
     """Catalogue record for a museum or gallery exhibit."""
 
+    class CurrencyChoices(models.TextChoices):
+        USD = 'USD', 'USD'
+        EUR = 'EUR', 'EUR'
+        GBP = 'GBP', 'GBP'
+
     id = models.AutoField(primary_key=True)
-    gallery_name = models.TextField(default='Hargreaves Fine Art')
-    show_name = models.TextField(db_column='show')
-    artwork = models.TextField()
-    artist = models.TextField()
-    medium = models.TextField()
-    dimensions_height = models.IntegerField()
-    dimensions_width = models.IntegerField()
+    gallery_name = models.CharField(max_length=100, default='Hargreaves Fine Art')
+    show_name = models.CharField(max_length=200, db_column='show')
+    artwork = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    medium = models.CharField(max_length=255)
+    dimensions_height = models.PositiveSmallIntegerField(help_text='Height in centimetres (cm)')
+    dimensions_width = models.PositiveSmallIntegerField(help_text='Width in centimetres (cm)')
     provenance = models.TextField()
-    price = models.IntegerField()
-    currency = models.TextField()
+    price = models.PositiveIntegerField()
+    currency = models.CharField(max_length=3, choices=CurrencyChoices.choices, default=CurrencyChoices.USD)
     tldr = models.TextField()
     full_text = models.TextField()
     audio_url = models.URLField()
     video_url = models.URLField()
+    image = models.ImageField(upload_to='exhibit_images/', null=True, blank=True)
     image_url = models.URLField()
-    qr_identifier = models.IntegerField()
+    qr_identifier = models.PositiveIntegerField(db_index=True, help_text='Printed QR code identifier')
     publish_date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='exhibits')
 
@@ -37,13 +43,13 @@ class Prospect(models.Model):
     """Gallery visitor that is interested in an exhibit."""
 
     id = models.AutoField(primary_key=True)
-    firstname = models.TextField()
-    lastname = models.TextField()
+    firstname = models.CharField(max_length=150)
+    lastname = models.CharField(max_length=150)
     email = models.EmailField()
-    phone = models.TextField()
+    phone = models.CharField(max_length=30, help_text='Phone number in international or local format')
     exhibit = models.ForeignKey(Exhibit, on_delete=models.CASCADE, related_name='prospects')
     saved_at = models.DateTimeField(auto_now_add=True)
-    dwell_time = models.IntegerField()
+    dwell_time = models.PositiveIntegerField()
 
     class Meta:
         db_table = 'prospects'
